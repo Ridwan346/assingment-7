fd9fbd6969b9078d53bcb6aa6b4f10e9b2874da6
Ridwan346 <ridwanhossain955@gmail.com>
2025-12-19 09:49:37 +0600

    seventh commit

--- src/card.jsx
+++ src/card.jsx
@@ -6,7 +6,7 @@ const Card = ({tiket ,baytiket ,setbaytiket,remove}) => {
     let hook = use(tiket)
     console.log(hook)
     return (
-        <div className='lg:flex sm:flex sm:flex-col gap-2.5'>
+        <div className='lg:flex lg:flex-row sm:flex sm:flex-col gap-2.5'>
              <div className="grid lg:grid-cols-2 sm:grid-cols-1 lg:w-2/3 sm:w-full gap-4">
       {
         hook.map(item => (
